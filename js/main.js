ba9bb666dca427e78a6cb48324532bb3c28278f8
dannyandreev <danny.andreev@gmail.com>
2021-05-25 11:05:34 -0700

    console logs removed and page refresh on entry

--- js/main.js
+++ js/main.js
@@ -9,17 +9,14 @@ form.addEventListener('submit', handleSubmit);
 var imgURL = '';
 
 function handleInput(event) {
-  // imgURL += event.target
   if (/\.(jpg|png|gif)$/.test(event.target.value)) {
     imgURL = event.target.value;
-    // console.log(imgURL)
-    // console.log(document.getElementById('journalCoverImage').getAttribute('src'))
     document.getElementById('journalCoverImage').setAttribute('src', imgURL);
   }
 }
 
 function handleSubmit(event) {
-  // event.preventDefault();
+  event.preventDefault();
   var journalEntry = makeJournalEntry(form.elements.title.value, form.elements.photoURL.value, form.elements.notes.value, data.nextEntryId - 1, data.nextEntryId);
   data.entries[data.nextEntryId - 1] = journalEntry;
   data.nextEntryId++;
